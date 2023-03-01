64600d5d518d8a3cdae0d8bdc6620f1f4d8221b7
nakamurakko <28667444+nakamurakko@users.noreply.github.com>
2023-03-01 09:48:37 +0900

    コード整理。

--- src/preload.ts
+++ src/preload.ts
@@ -3,22 +3,6 @@
 
 import { contextBridge, ipcRenderer } from 'electron';
 
-window.addEventListener("DOMContentLoaded", () => {
-  const replaceText = (selector: string, text: string) => {
-    const element = document.getElementById(selector);
-    if (element) {
-      element.innerText = text;
-    }
-  };
-
-  for (const type of ["chrome", "node", "electron"]) {
-    let version = process.versions[type as keyof NodeJS.ProcessVersions];
-    if (version) {
-      replaceText(`${type}-version`, version);
-    }
-  }
-});
-
 contextBridge.exposeInMainWorld('GreetingsAPI', {
   /**
    * 挨拶を返す。
